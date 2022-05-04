011609662dc215c69b57fa861924ae495f6b157c
Vladislav Kuznetsov <Vladislav.Kuznetsov@jetbrains.com>
2022-05-04 15:59:08 +0300

    Remove vcpkg

--- src/example.cpp
+++ src/example.cpp
@@ -2,12 +2,11 @@
 // Created by Vladislav.Kuznetsov on 04.05.22.
 //
 #include <iostream>
-#include <sqlite3.h>
 #include "../include/example.h"
 
 
 namespace vcpkgexample {
     void vcpkgexample::VCPKGExampleStruct::get_sql_version() {
-        std::cout << sqlite3_libversion();
+        std::cout << "SQL version - 3.0";
     }
 }
